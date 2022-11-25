a689838b37b6158b0881c748d731fb28ba3e01e0
Orlando <galunggong14@gmail.com>
2022-11-25 09:10:37 -0800

    php table

--- createdb.php
+++ createdb.php
@@ -1,35 +1,22 @@
 <?php
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-    $conn = mysqli_connect($servername, $username, $password);
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-    if(!$conn){
-        die("Connection failed" . mysqli_connect_error());
-    }
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
 
-    echo "Connected successfully";
-
-    $sql = "CREATE DATABASE myDB";
-    if(mysqli_query($conn, $sql)){
-        echo "Database created successfully";
-    } else {
-        echo "Error creating databse" . mysqli_error($conn);
-    }
-
-    $sql = "CREATE TABLE MyGuests (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        firstname VARCHAR(30) NOT NULL,
-        lastname VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        reg_date TIMESTAMP DEFAULT CURRNET_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )"
-
-    if (mysqli_query($conn, $sql)){
-        echo "Table MyGuests created successfully";
-    } else {
-        echo "Error creating table: " .mysqli_error($conn);
-    }
-    mysqli_close($conn);
+$conn->close();
 ?>
