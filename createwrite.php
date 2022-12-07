476ec989122796fff2086243dc8422d59ef2e8c9
Orlando <galunggong14@gmail.com>
2022-12-07 00:13:51 -0800

    create and write

---
+++ createwrite.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+
+<?php
+    $myfile = fopen("newfile.txt", "r") or die("Unable to open file.");
+    while(!feof($myfile)){
+        echo fgets($myfile). "<br>";
+    }
+
+    fclose($myfile);
+?>
+
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "Mickey Mouse\n";
+fwrite($myfile, $txt);
+$txt = "Minnie Mouse\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+</body>
+</html>
--- openread.php
+++ openread.php
@@ -7,13 +7,20 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $myfile = fopen("text.txt") or die("Unable to open file");
-        while(!feof($myfile)){
-            echo fgets($myfile) . "<br>";
-        }
+    
+<?php
+//$myfile = fopen("text.txt", "r") or die("Unable to open file!");
+//echo fgets($myfile);
+//fclose($myfile);
+?>
 
-        fclose($myfile);
-    ?>
+<?php
+$myfile = fopen("text.txt", "r") or die("Unable to open");
+while(!feof($myfile)){
+    echo fgets($myfile) . "<br>";
+}
+
+fclose($myfile);
+?>
 </body>
 </html>
