29f43bcecd03666fd015a09fb347dc6ab9576ea6
Orlando <galunggong14@gmail.com>
2022-12-12 22:32:17 -0800

    filter advance

--- filter.php
+++ filter.php
@@ -27,6 +27,60 @@ th, td {
     echo '<tr><td>' . $filter . '</td><td>' . filter_id($filter) . '</td></tr>';
   }
   ?>
+
+  <?php
+    $int = 0;
+    
+    if(filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT)
+    === flase){
+      echo("$int Integer is valid");
+    } else {
+      echo("Integer is not valid");
+    }
+  ?>
+<br>
+  <?php
+    $ip = "127.0.0.1";
+    if(!filter_var($ip, FILTER_VALIDATE_IP) === false){
+      echo "$ip is a valid IP address";
+    } else {
+      echo "$ip is not a valid IP address";
+
+    }
+
+  ?>
+<br>
+<?php
+$email = "john.doe@example.com";
+
+// Remove all illegal characters from email
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+// Validate e-mail
+if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+  echo("$email is a valid email address");
+} else {
+  echo("$email is not a valid email address");
+}
+?>
+
+<br>
+<?php
+$url = "https://www.w3schools.com";
+
+// Remove all illegal characters from a url
+$url = filter_var($url, FILTER_SANITIZE_URL);
+
+// Validate url
+if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
+  echo("$url is a valid URL");
+} else {
+  echo("$url is not a valid URL");
+}
+
+?>
+
+
 </table>
 </body>
 </html>
