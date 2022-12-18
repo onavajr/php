3502d97dad973a8eb89e94436f04f8e123481066
Orlando <galunggong14@gmail.com>
2022-12-17 23:07:04 -0800

    php json

---
+++ phpjson.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+echo json_encode($age);
+?>
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+
+echo json_encode($cars);
+?>
+
+<?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+ 
+var_dump(json_decode($jsonobj));
+?>
+
+
+</body>
+</html>
